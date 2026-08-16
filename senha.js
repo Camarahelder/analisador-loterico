d2fbd4edabf3a75221ec48a8e9dd296941ddd5fb
Camarahelder <136136686+Camarahelder@users.noreply.github.com>
2026-08-16 12:43:12 -0300

    Update senha.js

--- senha.js
+++ senha.js
@@ -1,6 +1,6 @@
 const CLIENTES = [
   { senha:'1809', tipo:'completo', nome:'Acesso Completo', expira: null },
   { senha:'3344', tipo:'completo', nome:'Acesso Completo', expira: null },
-  { senha:'1012', tipo:'trial',    nome:'Teste 2 dias',    expira: '2026-07-24' },
-  { senha:'2031', tipo:'trial',    nome:'Teste 30 dias',   expira: '2026-07-27' },
+  { senha:'1012', tipo:'trial',    nome:'Teste 2 dias',    expira: '2026-08-18' },
+  { senha:'2030', tipo:'trial',    nome:'Teste 30 dias',   expira: '2026-09-17' },
 ];
